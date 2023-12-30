06cb5b71081be72bd0e2e30ba208e380c62fb9c2
Michael Windon <michael.windon@gmail.com>
2023-12-30 09:39:07 -0600

    Add Menu

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { ThemeProvider} from "@aws-amplify/ui-react";
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import { ThemeProvider } from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
 
-import "@aws-amplify/ui-react/styles.css";
+import '@aws-amplify/ui-react/styles.css'
 
-import { studioTheme } from "./ui-components";
+import { studioTheme } from './ui-components'
 
-Amplify.configure(awsconfig);
-
+Amplify.configure(awsconfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={studioTheme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={studioTheme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 )
