0b89a46a17de6117da0efa2b41ce1bbc619a6465
Michael Windon <michael.windon@gmail.com>
2024-01-02 15:55:28 -0600

    Clone from Dev - Minimal Vialable Project Merge

--- src/ui-components/GroupModelUpdateForm.jsx
+++ src/ui-components/GroupModelUpdateForm.jsx
@@ -6,182 +6,10 @@
 
 /* eslint-disable */
 import * as React from "react";
-import {
-  Autocomplete,
-  Badge,
-  Button,
-  Divider,
-  Flex,
-  Grid,
-  Icon,
-  ScrollView,
-  Text,
-  TextField,
-  useTheme,
-} from "@aws-amplify/ui-react";
-import { GroupModel, ManagerModel, GroupModelManagerModel } from "../models";
-import {
-  fetchByPath,
-  getOverrideProps,
-  useDataStoreBinding,
-  validateField,
-} from "./utils";
+import { Button, Flex, Grid, TextField } from "@aws-amplify/ui-react";
+import { GroupModel } from "../models";
+import { fetchByPath, getOverrideProps, validateField } from "./utils";
 import { DataStore } from "aws-amplify/datastore";
-function ArrayField({
-  items = [],
-  onChange,
-  label,
-  inputFieldRef,
-  children,
-  hasError,
-  setFieldValue,
-  currentFieldValue,
-  defaultFieldValue,
-  lengthLimit,
-  getBadgeText,
-  runValidationTasks,
-  errorMessage,
-}) {
-  const labelElement = <Text>{label}</Text>;
-  const {
-    tokens: {
-      components: {
-        fieldmessages: { error: errorStyles },
-      },
-    },
-  } = useTheme();
-  const [selectedBadgeIndex, setSelectedBadgeIndex] = React.useState();
-  const [isEditing, setIsEditing] = React.useState();
-  React.useEffect(() => {
-    if (isEditing) {
-      inputFieldRef?.current?.focus();
-    }
-  }, [isEditing]);
-  const removeItem = async (removeIndex) => {
-    const newItems = items.filter((value, index) => index !== removeIndex);
-    await onChange(newItems);
-    setSelectedBadgeIndex(undefined);
-  };
-  const addItem = async () => {
-    const { hasError } = runValidationTasks();
-    if (
-      currentFieldValue !== undefined &&
-      currentFieldValue !== null &&
-      currentFieldValue !== "" &&
-      !hasError
-    ) {
-      const newItems = [...items];
-      if (selectedBadgeIndex !== undefined) {
-        newItems[selectedBadgeIndex] = currentFieldValue;
-        setSelectedBadgeIndex(undefined);
-      } else {
-        newItems.push(currentFieldValue);
-      }
-      await onChange(newItems);
-      setIsEditing(false);
-    }
-  };
-  const arraySection = (
-    <React.Fragment>
-      {!!items?.length && (
-        <ScrollView height="inherit" width="inherit" maxHeight={"7rem"}>
-          {items.map((value, index) => {
-            return (
-              <Badge
-                key={index}
-                style={{
-                  cursor: "pointer",
-                  alignItems: "center",
-                  marginRight: 3,
-                  marginTop: 3,
-                  backgroundColor:
-                    index === selectedBadgeIndex ? "#B8CEF9" : "",
-                }}
-                onClick={() => {
-                  setSelectedBadgeIndex(index);
-                  setFieldValue(items[index]);
-                  setIsEditing(true);
-                }}
-              >
-                {getBadgeText ? getBadgeText(value) : value.toString()}
-                <Icon
-                  style={{
-                    cursor: "pointer",
-                    paddingLeft: 3,
-                    width: 20,
-                    height: 20,
-                  }}
-                  viewBox={{ width: 20, height: 20 }}
-                  paths={[
-                    {
-                      d: "M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z",
-                      stroke: "black",
-                    },
-                  ]}
-                  ariaLabel="button"
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    removeItem(index);
-                  }}
-                />
-              </Badge>
-            );
-          })}
-        </ScrollView>
-      )}
-      <Divider orientation="horizontal" marginTop={5} />
-    </React.Fragment>
-  );
-  if (lengthLimit !== undefined && items.length >= lengthLimit && !isEditing) {
-    return (
-      <React.Fragment>
-        {labelElement}
-        {arraySection}
-      </React.Fragment>
-    );
-  }
-  return (
-    <React.Fragment>
-      {labelElement}
-      {isEditing && children}
-      {!isEditing ? (
-        <>
-          <Button
-            onClick={() => {
-              setIsEditing(true);
-            }}
-          >
-            Add item
-          </Button>
-          {errorMessage && hasError && (
-            <Text color={errorStyles.color} fontSize={errorStyles.fontSize}>
-              {errorMessage}
-            </Text>
-          )}
-        </>
-      ) : (
-        <Flex justifyContent="flex-end">
-          {(currentFieldValue || isEditing) && (
-            <Button
-              children="Cancel"
-              type="button"
-              size="small"
-              onClick={() => {
-                setFieldValue(defaultFieldValue);
-                setIsEditing(false);
-                setSelectedBadgeIndex(undefined);
-              }}
-            ></Button>
-          )}
-          <Button size="small" variation="link" onClick={addItem}>
-            {selectedBadgeIndex !== undefined ? "Save" : "Add"}
-          </Button>
-        </Flex>
-      )}
-      {arraySection}
-    </React.Fragment>
-  );
-}
 export default function GroupModelUpdateForm(props) {
   const {
     id: idProp,
@@ -197,73 +25,35 @@ export default function GroupModelUpdateForm(props) {
   const initialValues = {
     fullname: "",
     abbreviatedname: "",
-    Managers: [],
   };
   const [fullname, setFullname] = React.useState(initialValues.fullname);
   const [abbreviatedname, setAbbreviatedname] = React.useState(
     initialValues.abbreviatedname
   );
-  const [Managers, setManagers] = React.useState(initialValues.Managers);
   const [errors, setErrors] = React.useState({});
   const resetStateValues = () => {
     const cleanValues = groupModelRecord
-      ? { ...initialValues, ...groupModelRecord, Managers: linkedManagers }
+      ? { ...initialValues, ...groupModelRecord }
       : initialValues;
     setFullname(cleanValues.fullname);
     setAbbreviatedname(cleanValues.abbreviatedname);
-    setManagers(cleanValues.Managers ?? []);
-    setCurrentManagersValue(undefined);
-    setCurrentManagersDisplayValue("");
     setErrors({});
   };
   const [groupModelRecord, setGroupModelRecord] =
     React.useState(groupModelModelProp);
-  const [linkedManagers, setLinkedManagers] = React.useState([]);
-  const canUnlinkManagers = false;
   React.useEffect(() => {
     const queryData = async () => {
       const record = idProp
         ? await DataStore.query(GroupModel, idProp)
         : groupModelModelProp;
       setGroupModelRecord(record);
-      const linkedManagers = record
-        ? await Promise.all(
-            (
-              await record.Managers.toArray()
-            ).map((r) => {
-              return r.managerModel;
-            })
-          )
-        : [];
-      setLinkedManagers(linkedManagers);
     };
     queryData();
   }, [idProp, groupModelModelProp]);
-  React.useEffect(resetStateValues, [groupModelRecord, linkedManagers]);
-  const [currentManagersDisplayValue, setCurrentManagersDisplayValue] =
-    React.useState("");
-  const [currentManagersValue, setCurrentManagersValue] =
-    React.useState(undefined);
-  const ManagersRef = React.createRef();
-  const getIDValue = {
-    Managers: (r) => JSON.stringify({ id: r?.id }),
-  };
-  const ManagersIdSet = new Set(
-    Array.isArray(Managers)
-      ? Managers.map((r) => getIDValue.Managers?.(r))
-      : getIDValue.Managers?.(Managers)
-  );
-  const managerModelRecords = useDataStoreBinding({
-    type: "collection",
-    model: ManagerModel,
-  }).items;
-  const getDisplayValue = {
-    Managers: (r) => `${r?.name ? r?.name + " - " : ""}${r?.id}`,
-  };
+  React.useEffect(resetStateValues, [groupModelRecord]);
   const validations = {
     fullname: [],
     abbreviatedname: [],
-    Managers: [],
   };
   const runValidationTasks = async (
     fieldName,
@@ -293,28 +83,19 @@ export default function GroupModelUpdateForm(props) {
         let modelFields = {
           fullname,
           abbreviatedname,
-          Managers,
         };
         const validationResponses = await Promise.all(
           Object.keys(validations).reduce((promises, fieldName) => {
             if (Array.isArray(modelFields[fieldName])) {
               promises.push(
                 ...modelFields[fieldName].map((item) =>
-                  runValidationTasks(
-                    fieldName,
-                    item,
-                    getDisplayValue[fieldName]
-                  )
+                  runValidationTasks(fieldName, item)
                 )
               );
               return promises;
             }
             promises.push(
-              runValidationTasks(
-                fieldName,
-                modelFields[fieldName],
-                getDisplayValue[fieldName]
-              )
+              runValidationTasks(fieldName, modelFields[fieldName])
             );
             return promises;
           }, [])
@@ -331,88 +112,11 @@ export default function GroupModelUpdateForm(props) {
               modelFields[key] = null;
             }
           });
-          const promises = [];
-          const managersToLinkMap = new Map();
-          const managersToUnLinkMap = new Map();
-          const managersMap = new Map();
-          const linkedManagersMap = new Map();
-          Managers.forEach((r) => {
-            const count = managersMap.get(getIDValue.Managers?.(r));
-            const newCount = count ? count + 1 : 1;
-            managersMap.set(getIDValue.Managers?.(r), newCount);
-          });
-          linkedManagers.forEach((r) => {
-            const count = linkedManagersMap.get(getIDValue.Managers?.(r));
-            const newCount = count ? count + 1 : 1;
-            linkedManagersMap.set(getIDValue.Managers?.(r), newCount);
-          });
-          linkedManagersMap.forEach((count, id) => {
-            const newCount = managersMap.get(id);
-            if (newCount) {
-              const diffCount = count - newCount;
-              if (diffCount > 0) {
-                managersToUnLinkMap.set(id, diffCount);
-              }
-            } else {
-              managersToUnLinkMap.set(id, count);
-            }
-          });
-          managersMap.forEach((count, id) => {
-            const originalCount = linkedManagersMap.get(id);
-            if (originalCount) {
-              const diffCount = count - originalCount;
-              if (diffCount > 0) {
-                managersToLinkMap.set(id, diffCount);
-              }
-            } else {
-              managersToLinkMap.set(id, count);
-            }
-          });
-          managersToUnLinkMap.forEach(async (count, id) => {
-            const recordKeys = JSON.parse(id);
-            const groupModelManagerModelRecords = await DataStore.query(
-              GroupModelManagerModel,
-              (r) =>
-                r.and((r) => {
-                  return [
-                    r.managerModelId.eq(recordKeys.id),
-                    r.groupModelId.eq(groupModelRecord.id),
-                  ];
-                })
-            );
-            for (let i = 0; i < count; i++) {
-              promises.push(DataStore.delete(groupModelManagerModelRecords[i]));
-            }
-          });
-          managersToLinkMap.forEach((count, id) => {
-            const managerModelToLink = managerModelRecords.find((r) =>
-              Object.entries(JSON.parse(id)).every(
-                ([key, value]) => r[key] === value
-              )
-            );
-            for (let i = count; i > 0; i--) {
-              promises.push(
-                DataStore.save(
-                  new GroupModelManagerModel({
-                    groupModel: groupModelRecord,
-                    managerModel: managerModelToLink,
-                  })
-                )
-              );
-            }
-          });
-          const modelFieldsToSave = {
-            fullname: modelFields.fullname,
-            abbreviatedname: modelFields.abbreviatedname,
-          };
-          promises.push(
-            DataStore.save(
-              GroupModel.copyOf(groupModelRecord, (updated) => {
-                Object.assign(updated, modelFieldsToSave);
-              })
-            )
+          await DataStore.save(
+            GroupModel.copyOf(groupModelRecord, (updated) => {
+              Object.assign(updated, modelFields);
+            })
           );
-          await Promise.all(promises);
           if (onSuccess) {
             onSuccess(modelFields);
           }
@@ -436,7 +140,6 @@ export default function GroupModelUpdateForm(props) {
             const modelFields = {
               fullname: value,
               abbreviatedname,
-              Managers,
             };
             const result = onChange(modelFields);
             value = result?.fullname ?? value;
@@ -462,7 +165,6 @@ export default function GroupModelUpdateForm(props) {
             const modelFields = {
               fullname,
               abbreviatedname: value,
-              Managers,
             };
             const result = onChange(modelFields);
             value = result?.abbreviatedname ?? value;
@@ -477,84 +179,6 @@ export default function GroupModelUpdateForm(props) {
         hasError={errors.abbreviatedname?.hasError}
         {...getOverrideProps(overrides, "abbreviatedname")}
       ></TextField>
-      <ArrayField
-        onChange={async (items) => {
-          let values = items;
-          if (onChange) {
-            const modelFields = {
-              fullname,
-              abbreviatedname,
-              Managers: values,
-            };
-            const result = onChange(modelFields);
-            values = result?.Managers ?? values;
-          }
-          setManagers(values);
-          setCurrentManagersValue(undefined);
-          setCurrentManagersDisplayValue("");
-        }}
-        currentFieldValue={currentManagersValue}
-        label={"Managers"}
-        items={Managers}
-        hasError={errors?.Managers?.hasError}
-        runValidationTasks={async () =>
-          await runValidationTasks("Managers", currentManagersValue)
-        }
-        errorMessage={errors?.Managers?.errorMessage}
-        getBadgeText={getDisplayValue.Managers}
-        setFieldValue={(model) => {
-          setCurrentManagersDisplayValue(
-            model ? getDisplayValue.Managers(model) : ""
-          );
-          setCurrentManagersValue(model);
-        }}
-        inputFieldRef={ManagersRef}
-        defaultFieldValue={""}
-      >
-        <Autocomplete
-          label="Managers"
-          isRequired={false}
-          isReadOnly={false}
-          placeholder="Search ManagerModel"
-          value={currentManagersDisplayValue}
-          options={managerModelRecords
-            .filter((r) => !ManagersIdSet.has(getIDValue.Managers?.(r)))
-            .map((r) => ({
-              id: getIDValue.Managers?.(r),
-              label: getDisplayValue.Managers?.(r),
-            }))}
-          onSelect={({ id, label }) => {
-            setCurrentManagersValue(
-              managerModelRecords.find((r) =>
-                Object.entries(JSON.parse(id)).every(
-                  ([key, value]) => r[key] === value
-                )
-              )
-            );
-            setCurrentManagersDisplayValue(label);
-            runValidationTasks("Managers", label);
-          }}
-          onClear={() => {
-            setCurrentManagersDisplayValue("");
-          }}
-          onChange={(e) => {
-            let { value } = e.target;
-            if (errors.Managers?.hasError) {
-              runValidationTasks("Managers", value);
-            }
-            setCurrentManagersDisplayValue(value);
-            setCurrentManagersValue(undefined);
-          }}
-          onBlur={() =>
-            runValidationTasks("Managers", currentManagersDisplayValue)
-          }
-          errorMessage={errors.Managers?.errorMessage}
-          hasError={errors.Managers?.hasError}
-          ref={ManagersRef}
-          labelHidden={true}
-          {...getOverrideProps(overrides, "Managers")}
-        ></Autocomplete>
-      </ArrayField>
       <Flex
         justifyContent="space-between"
         {...getOverrideProps(overrides, "CTAFlex")}
--- src/ui-components/ManagerModelUpdateForm.d.ts
+++ src/ui-components/ManagerModelUpdateForm.d.ts
@@ -5,8 +5,8 @@
  **************************************************************************/
 
 import * as React from "react";
-import { AutocompleteProps, GridProps, SelectFieldProps, TextFieldProps } from "@aws-amplify/ui-react";
-import { ManagerModel, ContactCenterModel, GroupModel } from "../models";
+import { GridProps, SelectFieldProps, TextFieldProps } from "@aws-amplify/ui-react";
+import { ManagerModel } from "../models";
 export declare type EscapeHatchProps = {
     [elementHierarchy: string]: Record<string, unknown>;
 } | null;
@@ -28,8 +28,6 @@ export declare type ManagerModelUpdateFormInputValues = {
     telephone?: string;
     profilepic?: string;
     role?: string;
-    ContactCenters?: ContactCenterModel[];
-    Groups?: GroupModel[];
 };
 export declare type ManagerModelUpdateFormValidationValues = {
     name?: ValidationFunction<string>;
@@ -37,8 +35,6 @@ export declare type ManagerModelUpdateFormValidationValues = {
     telephone?: ValidationFunction<string>;
     profilepic?: ValidationFunction<string>;
     role?: ValidationFunction<string>;
-    ContactCenters?: ValidationFunction<ContactCenterModel>;
-    Groups?: ValidationFunction<GroupModel>;
 };
 export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
 export declare type ManagerModelUpdateFormOverridesProps = {
@@ -48,8 +44,6 @@ export declare type ManagerModelUpdateFormOverridesProps = {
     telephone?: PrimitiveOverrideProps<TextFieldProps>;
     profilepic?: PrimitiveOverrideProps<TextFieldProps>;
     role?: PrimitiveOverrideProps<SelectFieldProps>;
-    ContactCenters?: PrimitiveOverrideProps<AutocompleteProps>;
-    Groups?: PrimitiveOverrideProps<AutocompleteProps>;
 } & EscapeHatchProps;
 export declare type ManagerModelUpdateFormProps = React.PropsWithChildren<{
     overrides?: ManagerModelUpdateFormOverridesProps | undefined | null;
